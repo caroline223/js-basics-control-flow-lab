b9882af1023148972b58361ddaf70b348db58450
caroline223 <carolineforrester22@gmail.com>
2021-03-17 00:42:47 -0400

    commit tree f0025aa1cefcedae0ec034a47619a06960a1a583

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someDistance){
   // Write your code here!
+  let response;
+ 
+  if (someDistance <= 400)
+  {
+    response = 'This one is on me!'
+  } 
+  else if (someDistance > 2000) {
+    if (someDistance > 2500) {
+      response = 'No can do.'
+    }
+    else {
+      response = 'I will gladly take your thirty bucks.'
+    }
+  }
+    
+  return response
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(location){
   // Write your code here!
+ const response = location === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+
+ return response
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+
+  let response;
+
+  switch (tip) {
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+      case 'not as generous':
+        response = 'Thank you.'
+        break
+      default:
+        response = 'Bye.'
+
+  }
+  return response; 
 }
